f9aabecd3436c88aed49fd0e800301cab5bee43a
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2020-09-12 01:03:59 -0400

    Yes daddy

--- src/editor/MapEditor.java
+++ src/editor/MapEditor.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.BoxLayout;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,9 +23,13 @@ import javax.swing.SwingUtilities;
 
 // driver class
 public class MapEditor {
+	
+	// for external use
+	public static UI ui;
+	
 	public static void main(String[] args) {
 		// create ui
-		new UI();
+		ui = new UI();
 	}
 }
 
@@ -35,24 +38,35 @@ class UI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	// the window
-	private JFrame window;
+	public JFrame window;
 	
-	// the panels
-	public JPanel paintSurface, tileSet, help;
+	// stores tile map information
+	public Map map;
 	
-	// should the help panel be visible
-	private boolean hideHelp;
+	// stores tileset information
+	public TileSelector tileSelector;
 	
+	// stores helpful information
+	public Help help;
+	
+	// for map and tile selector classes to use
+	public BufferedImage tileset;
+	public Tile selectedTile;
+	public int tileSize;
+	
+	public ArrayList<BufferedImage> individualTiles;
+	
+	// ui object constructor
 	public UI() {
 		// create window
 		window = new JFrame();
 		
-		// adds the panels
-		window.getContentPane().add(paintSurface = new Map());
-		window.getContentPane().add(tileSet = new Tileset());
+		// adds the panels to the window
+		window.getContentPane().add(map = new Map());
+		window.getContentPane().add(tileSelector = new TileSelector());
 		window.getContentPane().add(help = new Help());
 		
-		// adds key listener
+		// adds key listener to the window
 		window.addKeyListener(new Keyboard());
 		
 		// defaults
@@ -63,81 +77,101 @@ class UI extends JFrame {
 		window.pack();
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
+		
+		// initialize lists
+		individualTiles = new ArrayList<BufferedImage>();
 	}
 	
-	// key event listener
-	private class Keyboard extends KeyAdapter {
+	// key event listener class
+	class Keyboard extends KeyAdapter {
 		// when the key gets pressed
 		@Override
 		public void keyPressed(KeyEvent e) {
-			// key code
+			// the key that was pressed
 			int keyCode = e.getKeyCode();
 			
 			switch(keyCode) {
-			
-			// F1 key
+			// f1: creates new project
 			case 112:
-				// opens file chooser
-				((Tileset) tileSet).openTileset();
-				break;
-			
-			// F2 key
-			case 113:
-				// shows or hides help panel based on boolean value
-				// resizes the tileset panel to account for the lost window size
-				hideHelp = !hideHelp;
-				if(hideHelp) {
-					window.getContentPane().remove(help);
-					tileSet.setPreferredSize(new Dimension(550 + 5, 720));
-				} else {
-					window.getContentPane().add(help);
-					tileSet.setPreferredSize(new Dimension(350, 720));
+				if(FileUtil.showOpenDialog()) {
+					// prompts the user for tile size
+					// displays input window
+					String prompt = JOptionPane.showInputDialog(null, "Please enter a number.", "Enter tile size", JOptionPane.INFORMATION_MESSAGE);
+					
+					// user must press "OK" on input window for input to be processed
+					// user's input cannot be blank
+					// regex command for numbers only
+					if(prompt != null && !prompt.isBlank() && prompt.matches("[0-9]+"))
+						MapEditor.ui.tileSize = Integer.parseInt(prompt);
+					
+					// this gets ignored if user presses "CANCEL" on empty input
+					else if(prompt != null)
+						JOptionPane.showMessageDialog(null, "Please enter numbers only.", "Error", JOptionPane.ERROR_MESSAGE);
+					
+					MapEditor.ui.tileSelector.cutUpTileset();
+					MapEditor.ui.tileSelector.repaint();
 				}
+				
+				break;
+			// f2: displays or hides the help panel
+			case 113:
+				// resizes panels based on boolean value
+				if(help.visible) {
+					window.getContentPane().remove(help);
+					tileSelector.setPreferredSize(new Dimension(555, 720));
+				}
+				
+				else {
+					window.getContentPane().add(help);
+					tileSelector.setPreferredSize(new Dimension(350, 720));
+				}
+				
+				// switch boolean value
+				help.visible = !help.visible;
+				
 				window.pack();
 				break;
 			
-			// F3 key
-			case 114:
-				// locks or unlocks tile set movement
-				((Tileset) tileSet).switchTileSetMobility();
+			// f4: switches between delete/add "modes"
+			case 115:
+				// delete mode stuff goes here
 				break;
 			
-			// F4 key
-			case 115:
-				// user input for tile size
-				String input = JOptionPane.showInputDialog(null, "Please enter a number.", "Enter tile size", JOptionPane.INFORMATION_MESSAGE);
-				
-				// regex for numbers only
-				if(input != null && !input.isBlank() && input.matches("[0-9]+"))
-					((Tileset) tileSet).setTileSize(Integer.parseInt(input));
-				// if user presses cancel
-				else if(input != null)
-					JOptionPane.showMessageDialog(null, "Please enter numbers only.", "Error", JOptionPane.ERROR_MESSAGE);
+			// f5: saves map to a file
+			case 116:
+				FileUtil.save(map);
+				break;
+			
+			// f6: loads map from a file
+			case 117:
+				FileUtil.load();
 				break;
 			}
 		}
 	}
 }
 
-// map class
 class Map extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
-	private ArrayList<Tile> tiles;
+	public ArrayList<Tile> tiles;
+	
+	public int tileMapOffsetX, tileMapOffsetY, tileMapX, tileMapY;
 	
 	public Map() {
 		// defaults
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		setPreferredSize(new Dimension(730, 720));
-		setBackground(Color.WHITE);
+		setBackground(Color.BLACK);
 		
 		// add mouse event listeners
 		Mouse mouse = new Mouse();
+		
 		addMouseMotionListener(mouse);
 		addMouseListener(mouse);
 		
 		// add description
-		add(new JLabel("Map"));
+		add(new JLabel("Map")).setForeground(Color.WHITE);
 		
 		// create list
 		tiles = new ArrayList<Tile>();
@@ -148,125 +182,110 @@ class Map extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
+		if(tiles.isEmpty())
+			return;
+		
 		for(Tile tile : tiles)
-			if(tile != null)
-				g.drawImage(tile.getSprite(), tile.getX(), tile.getY(), null);
+			g.drawImage(MapEditor.ui.individualTiles.get(tile.index), tile.tileMapX + tileMapX, tile.tileMapY + tileMapY, null);
+		
+		g.setColor(Color.RED);
 	}
 	
 	private class Mouse extends MouseAdapter {
+		private boolean mouseExited;
+		
 		@Override
 		public void mousePressed(MouseEvent e) {
-			if(e.getButton() == MouseEvent.BUTTON1) {
-				Tile tile = Tileset.getSelectedTile(e.getX(), e.getY());
-				
-				if(tile != null) {
-					tiles.add(tile);
-					repaint();
-					System.out.println(tiles.size());
-				}
+			if(mouseExited)
+				return;
+			
+			if(SwingUtilities.isRightMouseButton(e)) {
+				tileMapOffsetX = e.getX() - tileMapX;
+				tileMapOffsetY = e.getY() - tileMapY;
 			}
+			
+			if(SwingUtilities.isLeftMouseButton(e)) {
+				MapEditor.ui.selectedTile.setTileMapPosition((e.getX() - tileMapX) / MapEditor.ui.tileSize * MapEditor.ui.tileSize, (e.getY() - tileMapY) / MapEditor.ui.tileSize * MapEditor.ui.tileSize);
+				
+				for(int i = 0; i < tiles.size(); i++)
+					if(tiles.get(i).tileMapX == MapEditor.ui.selectedTile.tileMapX && tiles.get(i).tileMapY == MapEditor.ui.selectedTile.tileMapY) {
+						tiles.remove(tiles.get(i));
+						break;
+					}
+				
+				tiles.add((Tile) MapEditor.ui.selectedTile.clone());
+			}
+			
+			repaint();
 		}
 		
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			if(SwingUtilities.isLeftMouseButton(e)) {
-				Tile tile = Tileset.getSelectedTile(e.getX(), e.getY());
-				
-				if(tile != null) {
-					tiles.add(tile);
-					repaint();
-					System.out.println(tiles.size());
-				}
+			if(mouseExited)
+				return;
+			
+			if(SwingUtilities.isRightMouseButton(e)) {
+				tileMapX = (e.getX() - tileMapOffsetX) / MapEditor.ui.tileSize * MapEditor.ui.tileSize;
+				tileMapY = (e.getY() - tileMapOffsetY) / MapEditor.ui.tileSize * MapEditor.ui.tileSize;
 			}
+			
+			if(SwingUtilities.isLeftMouseButton(e)) {
+				MapEditor.ui.selectedTile.setTileMapPosition((e.getX() - tileMapX) / MapEditor.ui.tileSize * MapEditor.ui.tileSize, (e.getY() - tileMapY) / MapEditor.ui.tileSize * MapEditor.ui.tileSize);
+				
+				for(int i = 0; i < tiles.size(); i++)
+					if(tiles.get(i).tileMapX == MapEditor.ui.selectedTile.tileMapX && tiles.get(i).tileMapY == MapEditor.ui.selectedTile.tileMapY) {
+						tiles.remove(tiles.get(i));
+						break;
+					}
+				
+				tiles.add((Tile) MapEditor.ui.selectedTile.clone());
+			}
+			
+			repaint();
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			mouseExited = false;
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			mouseExited = true;
 		}
 	}
 }
 
-// tileset class
-class Tileset extends JPanel {
+class TileSelector extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
-	// for file selection
-	private JFileChooser fileChooser;
-	
-	// the tileset
-	private static BufferedImage tileSet;
-	
 	// tileset transform
-	private static int mouseOffsetX, mouseOffsetY, imageX, imageY;
+	public int tilesetOffsetX, tilesetOffsetY, tilesetX, tilesetY;
 	
-	// is the user allowed to move the tileset
-	private boolean isTileSetLocked;
-	
-	// the selected tile from the tileset
-	private static int tileSize, selectedX, selectedY;
-	
-	public Tileset() {
+	public TileSelector() {
 		// defaults
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		setPreferredSize(new Dimension(350, 720));
-		setBackground(Color.WHITE);
+		setBackground(Color.BLACK);
 		
 		// add description
-		add(new JLabel("Tileset"));
+		add(new JLabel("Tileset")).setForeground(Color.WHITE);;
+		
 		
 		Mouse mouse = new Mouse();
 		addMouseMotionListener(mouse);
 		addMouseListener(mouse);
-		
-		tileSize = 1;
-		
-		fileChooser = new JFileChooser();
 	}
 	
-	// opens file chooser
-	public void openTileset() {
-		// which button the user presses
-		int result = fileChooser.showOpenDialog(null);
+	public void cutUpTileset() {
+		int width = MapEditor.ui.tileset.getWidth() / MapEditor.ui.tileSize;
+		int height = MapEditor.ui.tileset.getHeight() / MapEditor.ui.tileSize;
 		
-		try {
-			// loads image if valid option
-			if(result == JFileChooser.APPROVE_OPTION)
-				tileSet = ImageIO.read(fileChooser.getSelectedFile());
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
+		for(int i = 0; i < height; i++)
+			for(int j = 0; j < width; j++)
+				MapEditor.ui.individualTiles.add(MapEditor.ui.tileset.getSubimage(j * MapEditor.ui.tileSize, i * MapEditor.ui.tileSize, MapEditor.ui.tileSize, MapEditor.ui.tileSize));
 		
-		// resets values if valid option
-		if(result == JFileChooser.APPROVE_OPTION) {
-			imageX = 0;
-			imageY = 0;
-			selectedX = 0;
-			selectedY = 0;
-			isTileSetLocked = false;
-		}
-		
-		repaint();
-	}
-	
-	// sets the tile size
-	// also resets values
-	public void setTileSize(int size) {
-		tileSize = size;
-		
-		imageX = 0;
-		imageY = 0;
-		selectedX = 0;
-		selectedY = 0;
-		
-		repaint();
-	}
-	
-	// switches tile set movement flag
-	public void switchTileSetMobility() {
-		isTileSetLocked = !isTileSetLocked;
-	}
-	
-	// returns selected tile
-	public static Tile getSelectedTile(int x, int y) {
-		if(tileSet == null || selectedX - imageX > tileSet.getWidth() || selectedX - imageX < 0 || selectedY - imageY > tileSet.getHeight() || selectedY - imageY < 0)
-			return null;
-		return new Tile(tileSet.getSubimage(selectedX - imageX, selectedY - imageY, tileSize, tileSize), -1, x / tileSize * tileSize, y / tileSize * tileSize);
+		MapEditor.ui.selectedTile = new Tile(0);
 	}
 	
 	// where stuff gets drawn
@@ -274,114 +293,185 @@ class Tileset extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		// draw tileset only if it exists
-		if(tileSet != null) {
-			g.drawImage(tileSet, imageX, imageY, null);
-			// draws box around tileset
-			g.drawRect(imageX, imageY, tileSet.getWidth(), tileSet.getHeight());
-		}
+		if(MapEditor.ui.tileset == null)
+			return;
+		
+		g.drawImage(MapEditor.ui.tileset, tilesetX, tilesetY, null);
+		// draws box around tileset
+		g.setColor(Color.WHITE);
+		g.drawRect(tilesetX, tilesetY, MapEditor.ui.tileset.getWidth(), MapEditor.ui.tileset.getHeight());
 		
 		// draw selected tile from tileset
 		g.setColor(Color.RED);
-		g.drawRect(selectedX, selectedY, tileSize, tileSize);
+		g.drawRect(MapEditor.ui.selectedTile.selectorX, MapEditor.ui.selectedTile.selectorY, MapEditor.ui.tileSize, MapEditor.ui.tileSize);
 	}
 	
 	// mouse event listener
 	private class Mouse extends MouseAdapter {
+		public boolean mouseExited;
+		
 		// when the mouse is dragged (aka pressed and moving)
 		@Override
 		public void mouseDragged(MouseEvent e) {
-			// if the tileset exists, move it around
-			if(tileSet != null) {
-				// if the tileset is allowed to move, move it around
-				if(!isTileSetLocked) {
-					imageX = (e.getX() - mouseOffsetX) / tileSize * tileSize;
-					imageY = (e.getY() - mouseOffsetY) / tileSize * tileSize;
-				}
-				
-				// move the selected tile marker
-				selectedX = e.getX() / tileSize * tileSize;
-				selectedY = e.getY() / tileSize * tileSize;
-				
-				repaint();
+			if(mouseExited || MapEditor.ui.tileset == null || MapEditor.ui.tileSize == 0)
+				return;
+			
+			if(SwingUtilities.isRightMouseButton(e)) {
+				tilesetX = (e.getX() - tilesetOffsetX) / MapEditor.ui.tileSize * MapEditor.ui.tileSize;
+				tilesetY = (e.getY() - tilesetOffsetY) / MapEditor.ui.tileSize * MapEditor.ui.tileSize;
 			}
+			
+			if(SwingUtilities.isLeftMouseButton(e)) {
+				MapEditor.ui.selectedTile.setSelectorPosition(e.getX() / MapEditor.ui.tileSize * MapEditor.ui.tileSize, e.getY() / MapEditor.ui.tileSize * MapEditor.ui.tileSize);
+				
+				int temp = ((MapEditor.ui.selectedTile.selectorY - tilesetY) / MapEditor.ui.tileSize) * (MapEditor.ui.tileset.getWidth() / MapEditor.ui.tileSize) + ((MapEditor.ui.selectedTile.selectorX - tilesetX) / MapEditor.ui.tileSize);
+				
+				if(temp < MapEditor.ui.individualTiles.size())
+					MapEditor.ui.selectedTile.index = temp;
+			}
+			
+			repaint();
 		}
 		
 		// when any mouse key is pressed
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// if the tileset exists, initialize the offsets and move the selected tile marker
-			if(tileSet != null) {
-				mouseOffsetX = e.getX() - imageX;
-				mouseOffsetY = e.getY() - imageY;
-				
-				selectedX = e.getX() / tileSize * tileSize;
-				selectedY = e.getY() / tileSize * tileSize;
-				
-				repaint();
+			if(mouseExited || MapEditor.ui.tileset == null || MapEditor.ui.tileSize == 0)
+				return;
+			
+			if(SwingUtilities.isRightMouseButton(e)) {
+				tilesetOffsetX = e.getX() - tilesetX;
+				tilesetOffsetY = e.getY() - tilesetY;
 			}
+			
+			if(SwingUtilities.isLeftMouseButton(e)) {
+				MapEditor.ui.selectedTile.setSelectorPosition(e.getX() / MapEditor.ui.tileSize * MapEditor.ui.tileSize, e.getY() / MapEditor.ui.tileSize * MapEditor.ui.tileSize);
+				
+				int temp = ((MapEditor.ui.selectedTile.selectorY - tilesetY) / MapEditor.ui.tileSize) * (MapEditor.ui.tileset.getWidth() / MapEditor.ui.tileSize) + ((MapEditor.ui.selectedTile.selectorX - tilesetX) / MapEditor.ui.tileSize);
+				
+				if(temp < MapEditor.ui.individualTiles.size())
+					MapEditor.ui.selectedTile.index = temp;
+			}
+			
+			repaint();
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			mouseExited = false;
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			mouseExited = true;
 		}
 	}
 }
 
-// help class
+// help panel class
 class Help extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
+	// should the help panel be visible
+	public boolean visible;
+	
+	// help panel object constructor
 	public Help() {
 		// defaults
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new FlowLayout(FlowLayout.LEFT));
 		setPreferredSize(new Dimension(200, 720));
-		setBackground(new Color(0xFCFCFC));
+		setBackground(Color.WHITE);
 		
 		// helpful information
 		add(new JLabel("Keyboard Help"));
-		add(new JLabel("F1: Open tileset image."));
-		add(new JLabel("F2: Hide/show \"Keyboard Help.\""));
-		add(new JLabel("F3: Lock/unlock tileset movement."));
-		add(new JLabel("F4: Set tile size."));
+		add(new JLabel("F1: New Project."));
+		add(new JLabel("F2: Show/hide \"Keyboard Help.\""));
+		add(new JLabel("F3: Delete/add tiles."));
+		
+		visible = true;
 	}
 }
 
-class Tile {
-	private BufferedImage sprite;
-	private int index, x, y, isSolid;
+// tile class
+class Tile implements Cloneable {
+	// index of tile on tile map
+	public int index;
 	
-	public Tile(BufferedImage tileSprite, int spriteIndex, int xPos, int yPos) {
-		sprite = tileSprite;
-		index = spriteIndex;
-		x = xPos;
-		y = yPos;
+	// the position of the tile in the tile selector
+	public int selectorX, selectorY;
+	
+	// the position of the tile in the tile map
+	public int tileMapX, tileMapY;
+	
+	// is the tile solid
+	// 0: not solid
+	// 1: solid
+	public int solid;
+	
+	// tile object constructor
+	public Tile(int index) {
+		this.index = index;
 	}
 	
-	public int getX() {
-		return x;
+	public void setSelectorPosition(int x, int y) {
+		selectorX = x;
+		selectorY = y;
 	}
 	
-	public int getY() {
-		return y;
+	public void setTileMapPosition(int x, int y) {
+		tileMapX = x;
+		tileMapY = y;
 	}
 	
-	public int getIndex() {
-		return index;
-	}
-	
-	public BufferedImage getSprite() {
-		return sprite;
-	}
-	
-	public void setIsSolid(boolean val) {
-		isSolid = val ? 1 : 0;
+	public Object clone() {
+		try {
+			return super.clone();
+		}
+		
+		catch(CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
 
-// for loading and saving map files
-class MapFileUtil {
-	public static void openMapFile() {
+// tile map utility class
+class FileUtil {
+	// file chooser
+	public static JFileChooser fileChooser;
+	
+	public static boolean showOpenDialog() {
+		// open once
+		if(MapEditor.ui.tileset != null)
+			return false;
+		
+		fileChooser = new JFileChooser();
+		
+		// show open dialog and store button information
+		int result = FileUtil.fileChooser.showOpenDialog(null);
+		
+		try {
+			// loads image if valid option
+			if(result == JFileChooser.APPROVE_OPTION) {
+				MapEditor.ui.tileset = ImageIO.read(FileUtil.fileChooser.getSelectedFile());
+				return true;
+			}
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	// loads tile map data from file
+	public static void load() {
 		
 	}
 	
-	public static void saveMapFile() {
-		
+	// saves tile map data to file
+	public static void save(Map map) {
+		for(Tile tile : map.tiles)
+			System.out.print(tile.index + " ");
 	}
 }
