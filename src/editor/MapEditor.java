b64273847d51f213fb222802956f3a55d050e70c
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2020-09-14 10:26:08 -0400

    Updated everyhing

--- src/editor/MapEditor.java
+++ src/editor/MapEditor.java
@@ -1,9 +1,5 @@
 package editor;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 // driver class
 public class MapEditor {
 	
@@ -13,22 +9,5 @@ public class MapEditor {
 	public static void main(String[] args) {
 		// create ui
 		 ui = new UI();
-		
-//		File file = new File("C:\\Users\\khrap\\Desktop\\theMap.txt");
-//		
-//		Scanner scanner = null;
-//		
-//		try {
-//			scanner = new Scanner(file);
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		while(scanner.hasNextLine()) {
-//			String data = scanner.nextLine();
-//			System.out.println(data);
-//		}
-//		
-//		scanner.close();
 	}
 }
